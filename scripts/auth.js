987829a4722781923c4a4ac096cb27e7faf3bb70
schowdish <siddu.chowdary@outlook.com>
2020-11-10 11:52:58 -0500

    Added delete user functionality

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
 
     // Your web app's Firebase configuration
     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -25,11 +25,11 @@ $(function() {
             let defaultPage = window.location.pathname;
             console.log(defaultPage);
             if (!defaultPage.includes('index')) {
-                window.location.href='index.html';
+                window.location.href = 'index.html';
             }
             //clear user info field
             $('#user').text('');
-        }else {
+        } else {
             console.log('User logged in');
             //set user info in home page after log in
             $('#user').text(user.email);
@@ -47,20 +47,20 @@ $(function() {
         //temp user Id for testing
         const userId = $('#loginEmail').val();
 
-        
+
         auth.createUserWithEmailAndPassword(email, password).then(cred => {
             console.log(cred.user);
-            window.location.href='home.html';
+            window.location.href = 'home.html';
             const userId = cred.user.uid;
             const userName = email.substr(0, email.indexOf('@'));
 
             function writeUserData(userId, name, email, stories) {
                 firebase.database().ref('users/' + userId).set({
-                  username: name,
-                  email: email,
-                  stories: stories
+                    username: name,
+                    email: email,
+                    stories: stories
                 });
-              }
+            }
             writeUserData(userId, userName, email, {});
             alert(cred.user);
         }).catch(error => {
@@ -79,10 +79,10 @@ $(function() {
         const email = $('#loginEmail').val();
         const password = $('#loginPwd').val();
 
-        
+
         auth.signInWithEmailAndPassword(email, password).then(cred => {
             console.log(cred.user);
-            window.location.href='home.html';
+            window.location.href = 'home.html';
         }).catch(error => {
             console.log('Login error: ' + error);
             $("#loginmsg").html(`<span style="color:red">${error.message}</span>`)
@@ -99,7 +99,7 @@ $(function() {
 
         auth.signOut().then(cred => {
             console.log('Signed Out');
-            window.location.href='index.html';
+            window.location.href = 'index.html';
         }).catch(error => {
             console.log('Logout Error: ' + error);
         })
@@ -111,22 +111,22 @@ $(function() {
 
 
     // var usersRef = ref.child("data");
-//     users.set({
-//     testUserOne: {
-//         date_of_birth: "June 23, 1912",
-//         full_name: "test One"
-//     },
-//     secondTest: {
-//         date_of_birth: "December 9, 1906",
-//         full_name: "Second Test"
-//     }
-// })
+    //     users.set({
+    //     testUserOne: {
+    //         date_of_birth: "June 23, 1912",
+    //         full_name: "test One"
+    //     },
+    //     secondTest: {
+    //         date_of_birth: "December 9, 1906",
+    //         full_name: "Second Test"
+    //     }
+    // })
 
 
 })
 
 // handle Forgot Password link
-$('#forgot-password').on('click', function() {
+$('#forgot-password').on('click', function () {
 
     // create modal for reset
     let resetModel =
@@ -169,16 +169,16 @@ $('#forgot-password').on('click', function() {
     $("#reset-close").on('click', closeModal);
 
     // register handler for reset button
-    $("#reset-pwd-button").off('click') 
+    $("#reset-pwd-button").off('click')
     $("#reset-pwd-button").on('click', function () {
         // alert('reset button clicked')
 
         // get email from the modal and send to firebase. Firebase sends email to reset password.
         const resetEmail = $('#reset-email').val();
         const auth = firebase.auth();
-        auth.sendPasswordResetEmail(resetEmail).then(function() {
+        auth.sendPasswordResetEmail(resetEmail).then(function () {
             console.log('email sent')
-        }).catch(function(error) {
+        }).catch(function (error) {
             console.log(error)
         });
 
@@ -194,14 +194,14 @@ const closeModal = () => {
 }
 
 // handle user info update
-$('#user').on('click', function() {
+$('#user').on('click', function () {
 
     // get logged in user
     let user = firebase.auth().currentUser;
 
     // create modal
     let userModal =
-    `<div id="user-modal" class="modal">
+        `<div id="user-modal" class="modal">
         <div class="modal-background"></div>
         <div class="modal-card">
             <header class="modal-card-head">
@@ -235,6 +235,7 @@ $('#user').on('click', function() {
 
                             <div class="field">
                                 <button id="user-update" class="button is-success">Update</button>
+                                <button id="user-delete" class="button is-success">Delete</button>
                             </div>
                         </form>
                     </div>
@@ -264,28 +265,44 @@ $('#user').on('click', function() {
 
         // update user email address
         if (user.email !== userEmail) {
-            user.updateEmail(userEmail).then(function() {
+            user.updateEmail(userEmail).then(function () {
                 console.log('user email updated')
                 // display email and update message 
                 $('#user').text(user.email);
                 $('#user-msg').text('User Updated')
-            }).catch(function(error) {
+            }).catch(function (error) {
                 console.log(error)
             });
         }
 
         // update user password
         if (userPwd !== "") {
-            user.updatePassword(userPwd).then(function() {
+            user.updatePassword(userPwd).then(function () {
                 console.log('password updated')
                 // display update message
                 $('#user-msg').text('User Updated')
-            }).catch(function(error) {
+            }).catch(function (error) {
                 console.log(error)
             });
         }
 
         // close modal
+        closeModal()
+    })
+
+    //register handler for delete account button
+    $("#user-delete").off('click');
+    $('#user-delete').on('click', function () {
+
+        let main_user = firebase.auth().currentUser;
+
+        main_user.delete().then(function () {
+            window.location.href = 'index.html';
+            // console.log('Delete successful');
+        }).catch(function (error) {
+            // console.log(error);
+        })
+
         closeModal()
     })
 })
