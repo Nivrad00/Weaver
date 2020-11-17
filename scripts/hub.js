bf1fde30b0d9cdad4e0167a0abeb1e2b2dc5e283
Liz Hensley <elizabethannehensley@gmail.com>
2020-11-17 02:49:06 -0500

    hub updates

--- scripts/hub.js
+++ scripts/hub.js
@@ -4,6 +4,15 @@ const hubSetup = async function() {
 
 }
 
+const sharedStoryTemplate = {
+    id: null,
+    image: null,
+    title: "",
+    description: "",
+    content: null,
+    author: ""
+}
+
 
 const getStories = async function() {
     let stories = [];
@@ -12,11 +21,21 @@ const getStories = async function() {
 
         for (const [key, value] of Object.entries(snapshot)) {
             var userStories = value.stories;
+            var user = value.username;
             if (!(userStories == undefined) && !(userStories == null)) {
                 for (const [key, value] of Object.entries(userStories)) {
                     if (value.isShared) {
+                        let story = {
+                            ...sharedStoryTemplate
+                        }   
+                        story.id = value.id;
+                        story.image = value.image;
+                        story.title = value.title;
+                        story.description = value.description;
+                        story.content = value.content;
+                        story.author = user;
                         //key is the id of the story.  Each story id has values: title, description, text, image, isShared, id
-                        stories.push([key, value.title, value.text]);
+                        stories.push(story);
                     }
                 }
             }
@@ -29,14 +48,14 @@ const getStories = async function() {
 
 const createStoryCard = function(story) {
     //story id is story[0]
-    let title = story[1];
-    let body = story[2];
+    let title = story.title;
+    let description = story.description;
 
-    console.log(title, body);
+    console.log(title, description);
 
     $("#hubStories").append($("<div>").addClass("storyCard").append(
         $("<h1>").addClass("title is-3").text(title)).append(
-            $("<p>").addClass("subtitle is-5").text(body)
+            $("<p>").addClass("subtitle is-5").text(description)
     ))
 }
 
--- scripts/user-data.js
+++ scripts/user-data.js
@@ -52,19 +52,42 @@ function saveHelper(e) {
 function shareHelper(e) {
     e.preventDefault();
 
-    const id = $("#save-story").data('story-id');
+    let id = $("#save-story").data('story-id');
 
     const userId = firebase.auth().currentUser.uid;
 
+    // get Delta content object (holds text + text formatting)
+    let content = editor.getContents();
+
+    let description = content.ops[0].insert.slice(0, 40);
+
     let title = $("#story-title").val();
     if (title == "") {
-        title = "untitled" + id;
+        title = "untitled";
     }
 
     if (id === undefined) {
-        //if no story is selected... need to handle this case?
-        console.log("no story selected")
+        // if story does not yet have id...
+
+        id = nextID;
+        updateStoryTab(id, title, description);
+
+        firebase.database().ref('users/' + userId + "/stories/" + id).push({
+            content: content,
+            title: title,
+            isShared: true,
+            description: description,
+            id: id
+        }).catch(error => {
+            console.log(error.message)
+        }).then(newRef => {
+            id = newRef.id;
+            firebase.database().ref('users/' + userId + "/stories/" + id + "/id").set(id);
+        })
+
     } else {
+        updateStoryTab(id, title, description);
+        //save the story to the database under userId/stories/(id of the story)
         firebase.database().ref('users/' + userId + "/stories/" + id).update({
             description: description,
             id: id,
@@ -75,6 +98,7 @@ function shareHelper(e) {
             console.log(error.message)
         })
     }
+
 }
 
 $(function() {
