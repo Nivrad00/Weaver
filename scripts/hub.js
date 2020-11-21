7361d4c383da7d07d48e3f71fbcf1b43490b103d
Liz Hensley <elizabethannehensley@gmail.com>
2020-11-21 01:35:44 -0500

    hub card styling

--- scripts/hub.js
+++ scripts/hub.js
@@ -28,13 +28,14 @@ const getStories = async function() {
                         let story = {
                             ...sharedStoryTemplate
                         }   
+                        // key is the id of the story.  Each story id has values: title, description, text, image, isShared, id
                         story.id = value.id;
                         story.image = value.image;
                         story.title = value.title;
                         story.description = value.description;
                         story.content = value.content;
                         story.author = user;
-                        //key is the id of the story.  Each story id has values: title, description, text, image, isShared, id
+
                         stories.push(story);
                     }
                 }
@@ -47,18 +48,48 @@ const getStories = async function() {
 }
 
 const createStoryCard = function(story) {
-    //story id is story[0]
-    let title = story.title;
-    let description = story.description;
 
-    console.log(title, description);
-
-    $("#hubStories").append($("<div>").addClass("storyCard").append(
+    /*$("#hubStories").append($("<div>").addClass("storyCard").append(
         $("<h1>").addClass("title is-4").text(title)).append(
             $("<p>").addClass("subtitle is-5").text(description)
-    ))
-}
+    ))*/
 
+    if (!story.image) {
+        $("#hubStories").append(`
+            <div class="storyCard" data-story-id="${story.id}">
+                <div class="media">
+                    <div class="media-content">
+                        <p class="title is-4">${story.title || "Untitled"}</p>
+                        <p class="subtitle is-6">${story.description}</p>
+                    </div>
+                </div>
+            </div>
+        `)
+    } else {
+        $("#hubStories").append(`
+            <div class="storyCard" data-story-id="${story.id}">
+                <div class="level">
+                    <div class="media-left">
+                        <figure class="image">
+                            <img src="${story.image}" class="cover" alt="${story.title || "Untitled"}">
+                        </figure>
+                    </div>
+                    <div class="media-content">
+                        <p class="title is-5">${story.title || "Untitled"}</p>
+                        <p class="description">${story.description}</p>
+                    </div>
+                </div>
+                <div class="level level-author line-top">
+                    <div class="level-left">
+                        <div class="level-item">
+                            <a class="author">${story.author}</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `)
+     }
+}
 
 
 
